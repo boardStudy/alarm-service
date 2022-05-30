3198b9e4125a5cb053621111314b367dcd8ec2e8
이호인 <lovi0714@kyonggi.ac.kr>
2022-05-31 01:04:43 +0900

    이메일 변경

--- src/main/java/com/alarmservice/EmailAlarmProperties.java
+++ src/main/java/com/alarmservice/EmailAlarmProperties.java
@@ -2,7 +2,7 @@ package com.alarmservice;
 
 public class EmailAlarmProperties {
 
-    public static final String FROM = "dahoonemailtest@gmail.com";
+    public static final String FROM = "alarmmanager714@gmail.com";
     public static final String TITLE = "%s님이 댓글을 남겼습니다.";
     public static final String CONTENT = "%s님이 \"%s\" (이)라고 댓글을 남겼습니다.";
 }
